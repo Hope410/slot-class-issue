7210ca0b97bc866e52af2324209758dfb6cef0c9
Hope410 <timurbabkin.pshh@gmail.com>
2022-07-04 11:51:30 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <HelloWorld class="hello-world">
+      <div slot="test"></div>
+    </HelloWorld>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
